e130128a43aa169a7e90b8c0769f9ab42468f799
Linlan Cai <liana255c@outlook.com>
2023-10-27 15:42:27 -0400

    create bin folder

--- CS608_CrimeMgmtSys/src/module-info.java
+++ CS608_CrimeMgmtSys/src/module-info.java
@@ -2,11 +2,11 @@
  * 
  */
 /**
- * @author linlanc
+ * @author linlan cai
  *
  */
 module CS608_CrimeMgmtSys {
 	// Export packages so they can be accessed by other modules
-	
+
 	// Require other modules that this module depends on
 }
