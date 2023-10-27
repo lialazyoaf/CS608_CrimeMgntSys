c6bc0fa7468e425a5f8b6b4756eac44784c579ab
Linlan Cai <liana255c@outlook.com>
2023-10-27 14:40:55 -0400

    test

---
+++ CS608_CrimeMgmtSys/src/module-info.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+/**
+ * @author linlanc
+ *
+ */
+module CS608_CrimeMgmtSys {
+	// Export packages so they can be accessed by other modules
+	
+	// Require other modules that this module depends on
+}
